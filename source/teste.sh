448ee67fb41a5a0913a7d33a312d4eacd0798c15
ahlrodrigues <ahlr_2000@yahoo.com>
2018-06-19 16:34:58 -0300

    Criação do doteamviwerpkg

--- source/teste.sh
+++ source/teste.sh
@@ -1,60 +1,59 @@
 #!/bin/bash
-plextxt="teste"
-plex=yes
+oteamviewerpkgtxt="teste"
+doteamviewerpkg=yes
 null=/dev/null
 permix="chmod +x"
 permi0="chmod 770"
 rcd=/etc/rc.d
+Downloads=/home/ahlr/Downloads
 
-# Iniciando do deamon Teamviewer
-if [ $teamviewerd == yes ]; then	
-    echo -e "$teamviewerdtxt"
-    echo
-    echo "Testando se o Teamviewer esta instalado"
-    if [ ! -f "$rcd/rc.teamviewerd" ]; then
-    echo
-    echo "O Teamviewer nao esta instalado!"
-    echo
-    if [ grep teamviewerd /etc/rc.d/rc.local > $null ]; then
-    $permix $rcd/rc.teamviewerd
-    $permi0 $rcd/rc.teamviewerd
-    $rcd/rc.teamviewerd start > $null
+# Automatiza o SlackBuild Teamviewer
+if [ $doteamviewerpkg == yes ]; then
+    echo -e "$doteamviewerpkgtxt"
+    clear
+    nome=teamviewer
+    formato=amd64.deb
+    cd $Downloads
+    if [ -e $nome*.tar.gz ] && [ -e $nome_*_$formato ]; then
+    tar zvxf $nome*.tar.gz
+    mv $nome_*$formato $nome
     else
-    echo "#Inicializando o deamon rc.teamviewerd" >> $rcd/rc.local
-    echo "if [ -x $rcd/rc.teamviewerd ]; then" >> $rcd/rc.local
-    echo "$rcd/rc.teamviewerd start" >> $rcd/rc.local
-    echo "fi" >> $rcd/rc.local
-    $permix $rcd/rc.teamviewerd
-    $permi0 $rcd/rc.teamviewerd
-    $rcd/rc.teamviewerd start > $null
-    sleep 5
+    echo $nome_*_$formato
+    echo
+    echo -e "$PINK Algo deu errado, o SlackBuild ou o Fonte não foi encontrado! $NC"
+    echo 
+    echo
+    echo
+    echo 
+    exit
     fi
-fi
-fi
-
-#Iniciando do deamon Plex
-if [ $plex == yes ]; then
-    echo -e "$plextxt"
     echo
-    echo "Testando se o Plex esta instalado"
-    if [ ! -f "$rcd/rc.plexmediaserver" ]; then
     echo
-    echo "O Plex nao esta instalado!"
     echo
-    if [ grep plexmediaserver /etc/rc.d/rc.local > $null ]; then
-    $permix $rcd/rc.plexmediaserver
-    $permi0 $rcd/rc.plexmediaserver
-    $rcd/rc.plexmediaserver start > $null
-    else 
-    echo "#Inicializando o deamon rc.plexmediaserver" >> $rcd/rc.local
-    echo "if [ -x $rcd/rc.plexmediaserver ]; then" >> $rcd/rc.local
-    echo "$rcd/rc.plexmediaserver start" >> $rcd/rc.local
-    echo "fi" >> $rcd/rc.local
-    $permix $rcd/rc.plexmediaserver
-    $permi0 $rcd/rc.plexmediaserver
-    $rcd/rc.plexmediaserver start > $null
-    fi
+    versao=`ls $nome/$nome_*$formato | awk -F '_' {'print \$2'}`
+    sed -i "s/VERSION:-.*/VERSION:-$versao}/g" $nome/$nome.SlackBuild
+    cd $nome
+    ./$nome.SlackBuild
+    echo
+    echo
+    echo
+    echo -e "$GREEN Vamos instalar o programa $BBROWN $nome? Y|N $NC"
+    read install
+    case $install in
+    Y|y)
+    upgradepkg --install-new /tmp/$nome*
+    ;;
+    N|n)
+    exit
+    echo -e "$GREEN Pacote disponível noa pasta /tmp $NC"
+    ;;
+    *)
+    echo
+    echo
+    echo
+    echo -e "$BRED Ok, algo deu errado! $NC"
+    exit
     sleep 5
+    ;;
+    esac
 fi
-fi
-
